e73765a2a9e6c6eb23df2d6cf324bf6414048914
brom5033 <sjajdcjddlsl@naver.com>
2023-09-12 20:50:51 +0900

    feat: 공통컴포넌트 개발

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+	border-radius: 20px;
+	border: 2px solid #8c8581;
+	background: #8c8581;
+	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+	color: #fff;
+	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	font-family: Jua;
+	font-size: 40px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+  padding: 25px 35px;
+`;
