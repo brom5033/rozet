5ac9590199c0e9abb438e8b134effa9aa3ff1f54
brom5033 <sjajdcjddlsl@naver.com>
2023-09-18 23:06:45 +0900

    style: import 사이에 여백넣기

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// pages
 import { Start } from './pages/Start';
 import { Random } from './pages/Random';
 import { Result } from './pages/Result';
