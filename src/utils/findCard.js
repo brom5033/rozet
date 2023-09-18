756ef9162c94db81b923d9f775a5482fd0a98696
brom5033 <sjajdcjddlsl@naver.com>
2023-09-18 22:15:30 +0900

    refactor: utils 주석추가

--- src/utils/findCard.js
+++ src/utils/findCard.js
@@ -1,3 +1,10 @@
+/**
+ * 같은 네임의 카드를 찾아준다
+ * 
+ * @param {Array} arr 
+ * @param {String} name 
+ * @returns {Array} 
+ */
 export const findCard = (arr, name) => {
 	return arr.filter((card) => card.name === name)[0];
 };
