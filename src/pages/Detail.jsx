d1d6e2c6f2902b61ef67cc0dbca2aa9e320f0e8e
brom5033 <sjajdcjddlsl@naver.com>
2024-01-01 15:38:16 +0900

    feat(detail): add card animation

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
@@ -10,6 +10,56 @@ export const Detail = () => {
 
 	const goToStart = () => {
 		navigate('/');
+	};
+
+	const [isAnimating, setIsAnimating] = useState(false);
+	const imageRef = useRef(null); // 이미지 참조 생성
+
+	const handleImageClick = () => {
+		if (!isAnimating) {
+			setIsAnimating(true);
+	
+			const startY = window.scrollY;  // 초기 스크롤 위치
+			const endY = startY + 300;      // 이미지가 아래로 이동할 거리
+	
+			let currentTime = 0;
+	
+			// 스크롤 애니메이션 함수
+			const animateScroll = (duration) => {
+				currentTime += 1 / 60;  // 애니메이션 프레임 업데이트
+	
+				const progress = currentTime / duration;  // 진행률 계산
+				const val = easeInOutQuad(progress);  // 부드러운 애니메이션을 위한 이징 함수
+	
+				window.scrollTo(0, startY + (endY - startY) * val);
+	
+				if (currentTime < duration) {
+					requestAnimationFrame(() => animateScroll(duration));
+				}
+			};
+	
+			// 애니메이션 실행
+			animateScroll(2);  // 2초 동안 애니메이션 실행
+		} else {
+			setIsAnimating(false);
+		}
+	};
+	
+	// 이징 함수 예시 (부드러운 애니메이션을 위해)
+	function easeInOutQuad(t) {
+		return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+	}
+	
+
+	const variants = {
+		initial: {
+			scale: 1,
+			y: 0,
+		},
+		animate: {
+			scale: 1.5, // 확대될 크기
+			y: 300, // 아래로 이동할 거리
+		},
 	};
 
 	return (
@@ -25,9 +75,19 @@ export const Detail = () => {
 						<TextColor>우선 타로 셔플에 대해 먼저 구경해볼까요?</TextColor> <br />
 					</Text>
 					<Text>
-							아래의 카드를 <TextColor>클릭</TextColor>해주세요!
-						</Text>
+						아래의 카드를 <TextColor>클릭</TextColor>해주세요!
+					</Text>
 				</TextWrap>
+				<CardWrap>
+					<AnimatedImage
+						ref={imageRef} // 참조 연결
+						src="/images/tarot-background2.jpg"
+						alt="backofcard"
+						variants={variants}
+						animate={isAnimating ? 'animate' : 'initial'}
+						onClick={handleImageClick}
+					/>
+				</CardWrap>
 			</BannerWrap>
 			<div style={{ display: 'flex', justifyContent: 'center' }}>
 				<motion.div
@@ -37,9 +97,7 @@ export const Detail = () => {
 						rotate: -90,
 						borderRadius: '100%',
 					}}
-				>
-					
-				</motion.div>
+				></motion.div>
 			</div>
 			<CardMix />
 		</MainContainer>
@@ -77,4 +135,16 @@ const Text = styled.p`
 
 const TextColor = styled.span`
 	color: #d4beb0;
+`;
+
+const CardWrap = styled.div`
+	position: absolute;
+	top: 75%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+const AnimatedImage = styled(motion.img)`
+	width: 80px;
+	height: 130px;
 `;
